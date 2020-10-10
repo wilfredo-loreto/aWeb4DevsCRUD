6594b7c387963763af70870f2cd964728b59ff56
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-10-10 19:09:55 -0400

    Finished static content of create document page

--- src/components/CreateDocument.js
+++ src/components/CreateDocument.js
@@ -67,7 +67,7 @@ class CreateDocument extends Component {
         </div>
         <div className="mainContainer2">
           <h1 className="title">CONTENT</h1>
-          <div className="blockContainer">
+          <div className="blockContainer dynamicContentSubtitle">
             <div className="subtitleContainer">
               <h2 className="subtitle">Subtitle</h2>
               <img src="/icon/close.svg" />
@@ -78,7 +78,7 @@ class CreateDocument extends Component {
               </div>
             </div>
           </div>
-          <div className="blockContainer">
+          <div className="blockContainer dynamicContentText">
             <div className="subtitleContainer">
               <h2 className="subtitle">Text</h2>
               <img src="/icon/close.svg" />
@@ -89,7 +89,7 @@ class CreateDocument extends Component {
               </div>
             </div>
           </div>
-          <div className="blockContainer">
+          <div className="blockContainer dynamicContentImage">
           <div className="subtitleContainer">
             <h2 className="subtitle">Image</h2>
             <img src="/icon/close.svg" />
@@ -109,9 +109,9 @@ class CreateDocument extends Component {
             </div>
           </div>
         </div>
-        <div className="blockContainer">
+        <div className="blockContainer dynamicContentList">
           <div className="subtitleContainer">
-            <h2 className="subtitle">Tags</h2>
+            <h2 className="subtitle">List</h2>
             <img src="/icon/close.svg" />
           </div>
           <div className="colContainer">
@@ -130,7 +130,43 @@ class CreateDocument extends Component {
             <div className="rowContainer lessMargin">
               <div className="addNewItem totalWidth">
                 <img src="/icon/plus.svg" className="plusImage" />
-                <span>ADD NEW TAG</span>
+                <span>ADD NEW LIST ITEM</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blockContainer dynamicContentReferences">
+          <div className="subtitleContainer">
+            <h2 className="subtitle">References</h2>
+            <img src="/icon/close.svg" />
+          </div>
+          <div className="colContainer">
+            <div className="rowContainer lessMargin">
+              <label className="numberlist">1.</label>
+              <span className="referenceText">Author: </span>
+              <input type="text" className="totalWidth" />
+              <span className="referenceText">Link: </span>
+              <input type="text" className="lessWidth" />
+            </div>
+            <div className="rowContainer lessMargin">
+              <label className="numberlist">2.</label>
+              <span className="referenceText">Author: </span>
+              <input type="text" className="totalWidth" />
+              <span className="referenceText">Link: </span>
+              <input type="text" className="lessWidth" />
+            </div>
+            <div className="rowContainer lessMargin">
+              <label className="numberlist">3.</label>
+              <span className="referenceText">Author: </span>
+              <input type="text" className="totalWidth" />
+              <span className="referenceText">Link: </span>
+              <input type="text" className="lessWidth" />
+            </div>
+            
+            <div className="rowContainer lessMargin">
+              <div className="addNewItem totalWidth">
+                <img src="/icon/plus.svg" className="plusImage" />
+                <span>ADD NEW LIST ITEM</span>
               </div>
             </div>
           </div>
